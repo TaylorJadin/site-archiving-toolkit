f177207734e836c47e3211f24ec30e52357e636a
Taylor Jadin <jadin.taylor@gmail.com>
2022-10-12 11:02:22 -0500

    Update archive.sh

--- archive.sh
+++ archive.sh
@@ -6,10 +6,9 @@ domain=`echo $url | cut -d '/' -f 3`
 if [[ ${url} == http* ]];
 	then
     	mkdir -p "$workdir/$domain/httrack/"
-		mkdir -p "$workdir/$domain/browsertrix-crawler/"
-		mkdir -p "$workdir/$domain/httrack/"
-		docker run -d -p 9037:9037 -v "$workdir/$domain/browsertrix-crawler/:/crawls/collections/" -it webrecorder/browsertrix-crawler crawl --url $url --generateWACZ --workers 8 --text
-		docker run -it --rm -v $workdir/$domain/httrack/:/data taylorjadin/httrack bash -c "httrack --robots=0 $url"
+	mkdir -p "$workdir/$domain/browsertrix-crawler/"
+	docker run -d -p 9037:9037 -v "$workdir/$domain/browsertrix-crawler/:/crawls/collections/" -it webrecorder/browsertrix-crawler crawl --url $url --generateWACZ --workers 8 --text
+	docker run -it --rm -v $workdir/$domain/httrack/:/data taylorjadin/httrack bash -c "httrack --robots=0 $url"
     else
     	echo "URL must start with http:// or https://"
         echo "Ex: ./archive.sh https://reclaimed.tech"
