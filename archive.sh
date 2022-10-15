79fa83910055ec275193bb264eb90cbf0f31384a
Taylor Jadin <jadin.taylor@gmail.com>
2022-10-15 00:30:46 -0500

    fix ors

--- archive.sh
+++ archive.sh
@@ -8,9 +8,16 @@ if [[ ${url} == http* ]];
 	docker build . -t archive-toolkit
     mkdir -p $workdir/$domain/httrack/
 	mkdir -p $workdir/$domain/webrecorder/
-	docker run -d -p 9037:9037 -v $workdir/$domain/webrecorder/:/crawls/collections/ -it archive-toolkit crawl --url $url --generateWACZ --workers 8 --text
-	docker run -it --rm -v $workdir/$domain/httrack/:/crawls/httrack archive-toolkit httrack --robots=0 --path /crawls/httrack $url
-	docker run -it --rm -v $workdir/$domain/httrack/:/crawls/httrack archive-toolkit grep -Rl 'crossorigin="' . | xargs sed -i.bak -E -e 's/ integrity="[^"]+"//g' -e 's/ crossorigin="[^"]+"//g'
+	
+	# start browsertrix and detach
+	docker run -d -p 9037:9037 -v $workdir/$domain/webrecorder/:/crawls/collections/output/ -it archive-toolkit crawl --url $url --generateWACZ --workers 8 --text --collection output
+	
+	# start httrack crawl
+	docker run -v $workdir/$domain/httrack/:/crawls/httrack archive-toolkit httrack --robots=0 --path /crawls/httrack $url
+    
+	# strip crossorigin and integrity retrictions out of html
+	docker run -v $workdir/$domain/httrack/:/crawls/httrack archive-toolkit find . -name "*.html" -exec sed -i -E -e 's/integrity="[^"]+"//g' -e 's/crossorigin="[^"]+"//g' {} \;
+
     else
     	echo "URL must start with http:// or https://"
         echo "Ex: ./archive.sh https://reclaimed.tech"
