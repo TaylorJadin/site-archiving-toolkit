41bf5d5d6fbd9613ac1af77941c58fe7bb99ca21
Taylor Jadin <jadin.taylor@gmail.com>
2022-10-14 22:57:49 -0500

    Update archive.sh

--- archive.sh
+++ archive.sh
@@ -10,6 +10,7 @@ if [[ ${url} == http* ]];
 	mkdir -p "$workdir/$domain/browsertrix-crawler/"
 	docker run -d -p 9037:9037 -v "$workdir/$domain/browsertrix-crawler/:/crawls/collections/" -it webrecorder/browsertrix-crawler crawl --url $url --generateWACZ --workers 8 --text
 	docker run -it --rm -v $workdir/$domain/httrack/:/data httrack bash -c "httrack --robots=0 $url"
+	docker run -it --rm -v $workdir/$domain/httrack/:/data grep -Rl 'crossorigin="' . | xargs sed -i.bak -E -e 's/ integrity="[^"]+"//g' -e 's/ crossorigin="[^"]+"//g'
     else
     	echo "URL must start with http:// or https://"
         echo "Ex: ./archive.sh https://reclaimed.tech"
