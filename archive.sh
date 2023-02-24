fbf21a0e6992b7f306bc06e729b2e81fb3386f8f
Taylor Jadin <jadin.taylor@gmail.com>
2023-02-24 12:15:34 -0600

    restructure

--- archive.sh
+++ archive.sh
@@ -5,22 +5,39 @@ domain=`echo $url | cut -d '/' -f 3`
 
 if [[ ${url} == http* ]];
 	then
-	docker build . -t archive-toolkit
+	docker build -f resources/Dockerfile -t archive-toolkit
+
+	# make working directories
     mkdir -p $workdir/$domain/httrack/
 	mkdir -p $workdir/$domain/webrecorder/
 	
-	# start browsertrix and detach
-	docker run -d -p 9037:9037 \
-	-v $workdir/$domain/webrecorder/:/crawls/collections/output/ -it archive-toolkit \
-	crawl --url $url --generateWACZ --workers 8 --text --collection output
+	# start browsertrix
+	docker run --name webrecorder -d --rm -p 9037:9037 \
+	-v $workdir/$domain/webrecorder/:/crawls/collections/archive/ -it archive-toolkit \
+	crawl --url $url --generateWACZ --workers 8 --text --collection archive
 	
 	# start httrack crawl
-	docker run -d -v $workdir/$domain/httrack/:/crawls/httrack archive-toolkit \
-	httrack --robots=0 --path /crawls/httrack $url
-    
-	# strip crossorigin and integrity retrictions out of html
-	docker run -v $workdir/$domain/httrack/:/crawls/httrack archive-toolkit \
-	find . -name "*.html" -exec sed -i -E -e 's/integrity="[^"]+"//g' -e 's/crossorigin="[^"]+"//g' {} \;
+	docker run --name httrack -d --rm -v $workdir/$domain/httrack/:/crawls/httrack archive-toolkit \
+	httrack --robots=0 --extra-log --verbose --path /crawls/httrack $url
+
+	# attach to httrack if its running
+	is_running=$(docker ps -q -f name="httrack")
+	if [ -n "$is_running" ]; then
+		docker attach --sig-proxy=false httrack
+		clear
+		echo "HTTrack completed its crawl." 
+	fi
+
+	# attach to browsertrix if its still running
+	is_running=$(docker ps -q -f name="webrecorder")
+	if [ -n "$is_running" ]; then
+		echo "Browsertrix Crawler is still working, attaching to container."
+		sleep 3
+		docker attach --sig-proxy=false webrecorder
+	fi
+
+	# Clean up httrack mirror and browsertrix crawl mirrors
+	docker run --name archive-toolkit --rm -v $workdir/$domain/:/crawls/ archive-toolkit /bin/bash /cleanup.sh
 
     else
     	echo "URL must start with http:// or https://"
