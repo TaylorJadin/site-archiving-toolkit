a29179e0e986d764aa075f99442500a33d481eff
Taylor Jadin <jadin.taylor@gmail.com>
2023-02-24 19:49:14 -0600

    powershell version

--- archive.ps1
+++ archive.ps1
@@ -4,7 +4,17 @@ $domain=([System.Uri]$url).Host -replace '^www\.'
 $now=Get-Date -UFormat '+%Y-%m-%dT%H%M%S'
 $crawldir=Join-Path -Path $workdir -ChildPath $domain-$now
 
+Write-Output "Getting ready..."
 docker.exe build -f resources/Dockerfile . -t archive-toolkit
 
 mkdir $crawldir\httrack
 mkdir $crawldir\webrecorder
+
+Clear-Host
+Write-Output "Starting Browsertrix Crawler and HTTrack..."
+
+# start browsertrix
+docker run --name webrecorder -d --rm -p 9037:9037 -v $crawldir/:/crawls -it archive-toolkit  /bin/bash /webrecorder.sh $url $domain $now
+
+# start httrack crawl
+docker run --name httrack -d --rm -v $crawldir/:/crawls archive-toolkit /bin/bash /httrack.sh $url $domain $now
