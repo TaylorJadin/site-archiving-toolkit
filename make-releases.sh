b0bfac91be7164e8b79f7b41a3e63b9b650352ab
Taylor Jadin <jadin.taylor@gmail.com>
2023-02-26 10:54:26 -0600

    releases

--- make-releases.sh
+++ make-releases.sh
@@ -5,15 +5,16 @@ linux-macos-release=releases/linux-macos-site-archiving-toolkit
 mkdir -p $linux-macos-release
 cp -r resources/ $linux-macos-release
 cp *.sh $linux-macos-release
+rm $linux-macos-release/make-releases.sh
 cd releases
 zip -r linux-macos-site-archiving-toolkit.zip .
 rm $linux-macos-release
 
 # Make Windows version
 windows-release=releases/windows-site-archiving-toolkit
-mkdir -p $linux-macos-release
-cp -r resources/ $linux-macos-release
-cp *.sh $linux-macos-release
+mkdir -p $windows-release
+cp -r resources/ $windows-release
+cp *.ps1 $windows-release
 cd releases
 zip -r windows-site-archiving-toolkit.zip .
 rm $windows-release
