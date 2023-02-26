d53f93980c146359b77e6e624e7f2a67de363513
Taylor Jadin <jadin.taylor@gmail.com>
2023-02-26 11:13:43 -0600

    fix release script

--- make-releases.sh
+++ make-releases.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 # Make Linux / macOS vesrion
-linux-macos-release="releases/linux-macos-site-archiving-toolkit"
-mkdir -p $linux-macos-release
-cp -r resources/ $linux-macos-release
-cp *.sh $linux-macos-release
-rm $linux-macos-release/make-releases.sh
+linux_macos_release=releases/linux-macos-site-archiving-toolkit
+mkdir -p $linux_macos_release
+cp -r resources $linux_macos_release
+cp *.sh $linux_macos_release
+rm $linux_macos_release/make-releases.sh
 cd releases
 zip -r linux-macos-site-archiving-toolkit.zip .
-rm $linux-macos-release
 
 # Make Windows version
-windows-release="releases/windows-site-archiving-toolkit"
-mkdir -p $windows-release
-cp -r resources/ $windows-release
-cp *.ps1 $windows-release
+windows_release=releases/windows-site-archiving-toolkit
+mkdir -p $windows_release
+cp -r resources $windows_release
+cp *.ps1 $windows_release
 cd releases
 zip -r windows-site-archiving-toolkit.zip .
-rm $windows-release
-
