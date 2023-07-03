680d10268e2bf85c66db26aa90c445dc2dbee3ad
Taylor Jadin <jadin.taylor@gmail.com>
2023-07-03 12:52:33 -0500

    lose httrack stuff

--- quit-crawlers.sh
+++ quit-crawlers.sh
@@ -3,15 +3,6 @@
 # kill screen session
 screen -S site-archiving-toolkit -X quit
 
-# stop httrack if its running
-is_running=`docker ps -q -f name="httrack"`
-if [ -n "$is_running" ]; then
-    docker stop httrack > /dev/null
-    echo "Succesfully quit HTTTrack."
-    else
-        echo "HTTrack is not running."
-fi
-
 # stop browsertrix if its running
 is_running=`docker ps -q -f name="webrecorder"`
 if [ -n "$is_running" ]; then
