437e751a1d6f3eaec891ca748d9cc48a930d4f93
Taylor Jadin <jadin.taylor@gmail.com>
2023-07-05 07:38:46 -0500

    status comand

--- status.sh
+++ status.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# screen -d -m watch -n 30 ./status.sh umw1.sh
+
 dirs=`ls crawls/ | wc -l`
 lines_in_file=$(wc -l < "$1")
 total=$(( $lines_in_file - 3 ))
 
-rm "**Progress**"*
+rm -f "**Progress**"*
 echo "Progress $dirs of $total" > "**Progress** $dirs of $total"
