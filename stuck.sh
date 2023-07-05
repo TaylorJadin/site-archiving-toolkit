cb30055fe09c89b93060dc00ac212bf4ba93a5b7
Taylor Jadin <jadin.taylor@gmail.com>
2023-07-05 11:24:27 -0500

    stuck sites

--- stuck.sh
+++ stuck.sh
@@ -6,3 +6,4 @@ https://juliagoestobeijing.umwblogs.org/ \
 https://importfolio.umwblogs.org/ \
 https://litjournalspring11.umwblogs.org/ \
 https://hist451s14.umwblogs.org/ \
+https://tags.umwblogs.org/ \
