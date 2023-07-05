4fe485829dd0f3b564d031a0904296c1d1bdb4c7
Taylor Jadin <jadin.taylor@gmail.com>
2023-07-05 14:07:34 -0500

    unstuck

--- stuck.sh
+++ stuck.sh
@@ -7,4 +7,7 @@ https://importfolio.umwblogs.org/ \
 https://litjournalspring11.umwblogs.org/ \
 https://hist451s14.umwblogs.org/ \
 https://tags.umwblogs.org/ \
-https://ashleygraceterrell.umwblogs.org
+https://ashleygraceterrell.umwblogs.org \
+https://hist383f12.umwblogs.org/ \
+https://rjwash.umwblogs.org/ \
+https://introws.umwblogs.org/ \
