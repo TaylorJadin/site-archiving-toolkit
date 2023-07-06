396cfb7df3999e58db1dde95e762dbd5ddc1270f
Taylor Jadin <jadin.taylor@gmail.com>
2023-07-06 16:15:24 -0500

    fix status

--- status.sh
+++ status.sh
@@ -8,7 +8,7 @@ if [ -z "$STY" ]; then exec screen -m -S status watch -n 10 "$0" "$@"; fi
 ls=`ls crawls/ | wc -l`
 dirs=$(( $ls - 1))
 lines_in_file=$(wc -l < "$1")
-total=$(( $lines_in_file - 3 ))
+total=$(( $lines_in_file - 2 ))
 
 rm -f crawls/"**Progress** "*
 echo "Progress: $dirs of $total" > crawls/"**Progress** $dirs of $total"
