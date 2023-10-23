9df1ccaac6dee6f2491470a715fdf038b1feaf67
Taylor Jadin <jadin.taylor@gmail.com>
2023-10-23 11:11:32 -0500

    docker check

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+docker_check=`docker version | grep "ERROR: Cannot connect to the Docker daemon"`
+if [ -n $docker_check ]; then
+echo "It looks like the Docker daemon has not been started. Check to see if Docker is installed and running."
+exit
+fi
+
 docker-compose -f resources/docker-compose.yml up -d
