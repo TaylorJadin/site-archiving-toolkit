8ae3c755f55203449d9d30ef770a98897bc0ae8c
Taylor Jadin <jadin.taylor@gmail.com>
2023-10-23 11:36:21 -0500

    check if running

--- archive.ps1
+++ archive.ps1
@@ -1,13 +1,31 @@
+# check if crawls are already running and exit if they are
+$is_running=docker ps -q -f name="httrack"
+if ($is_running) {
+    Write-Output "It looks like there is already a crawl running."
+	Write-Output "Either check on the status by running .\attach.ps1 or quit by running .\quit-crawlers.ps1"
+    exit
+    }
+
+$is_running=docker ps -q -f name="webrecorder"
+if ($is_running) {
+    Write-Output "It looks like there is already a crawl running."
+	Write-Output "Either check on the status by running .\attach.ps1 or quit by running .\quit-crawlers.ps1"
+    exit
+    }
+
+# set variables
 $url=$args[0]
 $workdir=Join-Path -Path $pwd -ChildPath "crawls"
 $domain=([System.Uri]$url).Host -replace '^www\.'
 $now=Get-Date -UFormat '+%Y-%m-%dT%H%M%S'
 $crawldir=Join-Path -Path $workdir -ChildPath $now-$domain
 
+# build containers
 Write-Output "Getting ready..."
 docker.exe build -f resources/Dockerfile.webrecorder . -t site-archiving-toolkit-webrecorder
 docker.exe build -f resources/Dockerfile.httrack . -t site-archiving-toolkit-httrack
 
+# make directories
 mkdir $crawldir\httrack
 mkdir $crawldir\webrecorder
 
@@ -20,6 +38,7 @@ docker run --name webrecorder -d --rm -p 9037:9037 -v $crawldir/:/output -it sit
 # start httrack crawl
 docker run --name httrack -d --rm -v $crawldir/:/output site-archiving-toolkit-httrack /bin/bash /httrack.sh $url $domain $now
 
+# attach to crawls as they run
 $is_running=docker ps -q -f name="httrack"
 if ($is_running) {
     docker attach --sig-proxy=false httrack
